1ed75b6e54941cf98287734f40ee77f6697bace6
Syyreign <spencerehart@hotmail.com>
2023-04-13 14:06:37 -0700

    Added the ability to play notes with the frequency taken from the GUI. Still need to make the notes connect when there are multiple of the same frequency.

--- src/rustnes.rs
+++ src/rustnes.rs
@@ -1,14 +1,18 @@
-use std::time::Duration;
-use rodio::{source::{Source}, OutputStream, Sink};
+use egui::{Color32};
 
 // Moved waves below rustnes to allow mod to play waves, and clear up main
 mod waves;
+
+mod synth;
 
 pub(crate) struct RustNES {
     // Test variable for the GUI. Displays currently selected files name
     pub(crate) _picked_path: Option<String>,
     pub(crate) _test_bool: bool,
     pub(crate) volume: f32,
+    pub(crate) synth: synth::Synth,
+    pub(crate) unselected_color: Color32,
+    pub(crate) selected_color: Color32,
 }
 
 impl Default for RustNES {
@@ -17,13 +21,14 @@ impl Default for RustNES {
             _picked_path: None,
             _test_bool: false,
             volume: 100.0,
+            synth: synth::Synth::default(),
+            unselected_color: Color32::from_rgb(100, 100, 100),
+            selected_color: Color32::from_rgb(80, 200, 80),
         }
     }
 }
 
-impl RustNES{
-
-    
+impl RustNES{  
     pub(crate) fn navigation_bar(&mut self, ui: &mut egui::Ui){
 
         egui::TopBottomPanel::top("navigation_menu")
@@ -85,7 +90,7 @@ impl RustNES{
 
     }
 
-    // The edit context menu
+    /// The edit context menu
     pub(crate) fn edit_menu(ui: &mut egui::Ui) {
         if ui.button("STUB").clicked() {
             println!("TODO!");
@@ -93,19 +98,19 @@ impl RustNES{
         }
     }
 
-    // The debug contect menu.
-    // This menu should only be visible in debug mode
+    /// The debug contect menu.
+    /// This menu should only be visible in debug mode
     pub(crate) fn debug_menu(ui: &mut egui::Ui) {
         if ui.button("Play NES Triangle").clicked(){
-            play_nes_triangle_wave(440.0);
+            synth::play_nes_triangle_wave(440.0);
         }
 
         if ui.button("Play NES Pulse").clicked(){
-            play_nes_pulse_wave(440.0);
+            synth::play_nes_pulse_wave(440.0);
         }
 
         if ui.button("Play NES Noise").clicked(){
-            play_nes_noise();
+            synth::play_nes_noise();
         }
     }
 
@@ -117,59 +122,39 @@ impl RustNES{
         .resizable(false)
         .min_height(25.0)
         .show_inside(ui, |ui| {
-        ui.horizontal_centered(|ui| {
-            if ui.button("Play").clicked(){
-                println!("TODO!");
-            }
-            if ui.button("Pause").clicked(){
-                println!("TODO!"); 
-            }
-            ui.add(egui::Slider::new(&mut self.volume, 0.0..=100.0).show_value(false));
-        });
+            ui.horizontal_centered(|ui| {
+                if ui.button("Play").clicked(){
+                    self.synth.play();
+                }
+                if ui.button("Pause").clicked(){
+                    println!("TODO!"); 
+                }
+                ui.add(egui::Slider::new(&mut self.volume, 0.0..=100.0).show_value(false));
+            });
         });
     }
-}
 
+    pub(crate) fn note_stepper(&mut self, ui: &mut egui::Ui){
 
+        ui.columns(8, |columns|{ 
 
-///
-/// Temporarily moved here
-/// Simple rodio sink to play an NES triangle wave
-/// 
-fn play_nes_triangle_wave(freq: f32){
-    let source = waves::NESTriangleWave::new(freq).take_duration(Duration::from_secs_f32(1.0));
+            for i in 0..self.synth.get_columns_len(){
 
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
+                let curr: &mut synth::SequenceColumn = &mut self.synth.sequence_columns[i];
 
-    sink.append(source);
-    sink.sleep_until_end();
-}
+                for j in (0..12).rev(){
+                    if columns[i].add(
+                    egui::Button::new("").
+                    fill(
+                        if curr.is_selected(j) {self.selected_color} 
+                        else { self.unselected_color}
+                    )).clicked(){
+                        curr.select(j);
+                        println!("{}",curr.get_index());
+                    };
+                }
+            }
+        });
 
-///
-/// Temporarily moved here
-/// Simple rodio sink to play an NES pulse wave
-/// 
-fn play_nes_pulse_wave(freq: f32){
-    let source = waves::NESPulseWave::new(freq, 0.5).take_duration(Duration::from_secs_f32(1.0));
-
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
-
-    sink.append(source);
-    sink.sleep_until_end();
-}
-
-///
-/// Temporarily moved here
-/// Simple rodio sink to play a sine wave
-/// 
-fn play_nes_noise(){
-    let source = waves::NESNoise::new().take_duration(Duration::from_secs_f32(1.0));
-
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
-
-    sink.append(source);
-    sink.sleep_until_end();
+    }
 }
--- src/rustnes/synth.rs
+++ src/rustnes/synth.rs
@@ -3,13 +3,93 @@ use rodio::{Sink, OutputStream, Source};
 
 use crate::rustnes::waves;
 
-struct Synth{
-
+pub(crate) struct Synth{
+    pub(crate) sequence_columns: Vec<SequenceColumn>,
+    pub(crate) current_position: u32,
 }
 
-// impl Default for Synth{
+impl Default for Synth{
+    fn default() -> Self {
+        Self {  
+            sequence_columns: vec![SequenceColumn::default(); 8],
+            current_position: 0,
+        }
+    }
+}
 
-// }
+impl Synth{
+
+    /// Returns the number of columns in the synth
+    pub(crate) fn get_columns_len(&self) -> usize{
+        self.sequence_columns.len()
+    }
+
+    pub(crate) fn play(&mut self){
+        for i in 0..self.get_columns_len(){
+            let raw_note = self.sequence_columns[i].get_index();
+            if raw_note != -1 {
+                let freq = self.get_frequency(raw_note + 69);
+
+                let source = waves::NESTriangleWave::new(freq).take_duration(Duration::from_secs_f32(0.25));
+
+                let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+                let sink = Sink::try_new(&stream_handle).unwrap();
+            
+                sink.append(source);
+                sink.sleep_until_end();
+            }
+        }
+    }
+
+    fn get_frequency(&self, note: i32) -> f32{
+        440.0 * f32::powf(2.0, (note as f32 - 69.0) / 12.0)
+    }
+}
+
+
+/// The column of each note, its bools are represented as a binary number
+/// to keep from needing a large array of bools.
+#[derive(Clone)]
+pub(crate) struct SequenceColumn {
+    column: u16,
+}
+
+impl Default for SequenceColumn {
+    fn default() -> Self {
+        Self { 
+            column: 0,
+        }
+    }
+}
+
+
+impl SequenceColumn{
+
+    /// Select the note pressed. ANDs the number to clear everything but the selected,
+    /// then XORs the number to create a toggle
+    pub(crate) fn select(&mut self, index: u16){
+        let b = 1 << index;
+
+        self.column &= b;
+        self.column ^= b;
+    }
+
+    /// Checks if the current note is selected
+    pub(crate) fn is_selected(&self, index: u16) -> bool{
+        self.column == 1 << index
+    }
+
+    /// Returns the index of the current columns note
+    /// If non selected, return -1
+    pub(crate) fn get_index(&self) -> i32{
+        let index = self.column.trailing_zeros();
+
+        if index == 16 {
+            return -1;
+        }
+        index as i32
+    }
+}
 
 
 ///
